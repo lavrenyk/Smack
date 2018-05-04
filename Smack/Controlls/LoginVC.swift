b1a6298acb55e2891c059e62018f966d5a90ed64
MoHapX <lavrenyk@gmail.com>
2018-05-05 01:12:46 +0300

    added LoginVC ui elements

---
+++ Smack/Controlls/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by MoHapX on 05.05.2018.
+//  Copyright © 2018 MoHapX. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closedPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
