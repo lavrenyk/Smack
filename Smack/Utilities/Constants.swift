c71b3bccd948bca3ab330e064e71a6829be3e141
MoHapX <lavrenyk@gmail.com>
2018-05-05 21:28:39 +0300

    added Api Service file

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,18 @@
 
 import Foundation
 
+typealias ComplitionHandler = (_ Success: Bool) -> ()
+
+// URLs
+let BASE_URL = "https://chatty-02.herokuapp.com/v1/"
+let REGISTER_URL = "\(BASE_URL)account/register"
+
 // Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND =  "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
