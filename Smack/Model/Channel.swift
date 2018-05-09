14fa0e15a56254aeb335cd226fde247927d25029
MoHapX <lavrenyk@gmail.com>
2018-05-09 21:37:15 +0300

    added channels service load

---
+++ Smack/Model/Channel.swift
@@ -0,0 +1,22 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by MoHapX on 08.05.2018.
+//  Copyright © 2018 MoHapX. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+}
+
+//struct Channel: Decodable {
+//    public private(set) var _id: String!
+//    public private(set) var name: String!
+//    public private(set) var description: String!
+//    public private(set) var __v: Int?
+//}
--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -11,12 +11,13 @@ import Foundation
 typealias ComplitionHandler = (_ Success: Bool) -> ()
 
 // URLs
-//let BASE_URL = "http://localhost:3005/v1/"
-let BASE_URL = "https://chatty-02.herokuapp.com/v1/"
+let BASE_URL = "http://localhost:3005/v1/"
+//let BASE_URL = "https://chatty-02.herokuapp.com/v1/"
 let REGISTER_URL = "\(BASE_URL)account/register"
 let LOGIN_URL = "\(BASE_URL)account/login"
 let ADD_USER_URL = "\(BASE_URL)user/add"
 let USER_BY_EMAIL_URL = "\(BASE_URL)user/byEmail/"
+let GET_CHANNELS_URL = "\(BASE_URL)channel/"
 
 // Colors
 let SMACK_PURPLE_PLACEHOLDER = #colorLiteral(red: 0.2588235294, green: 0.3294117647, blue: 0.7254901961, alpha: 0.5)
